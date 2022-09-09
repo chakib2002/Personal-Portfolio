c5477150b31226ab7d4e732f49710388ac82d0e0
chakib2002 <abdelmalek.feddal@worldlearningalgeria.org>
2022-09-09 10:47:40 -0500

    solve Build errors

--- components/SkillsPage/Section.js
+++ components/SkillsPage/Section.js
@@ -12,7 +12,7 @@ const Section = () => {
                 <SkillCard title="UI/UX" image="/ui-ux.png"/>
             </div>
             <div className='my-20 mx-5 space-y-10 md:space-y-0 md:flex md:justify-evenly '>
-                <h1 className='mx-3 text-center text-sm text-gray-300 md:text-left md:self-end md:mb-9 lg:w-[750px]'>Having good communication skills can lead to better collaboration and team cohesiveness. Being self-aware of one's strengths and weaknesses can improve one's accountability skills. And the result is a well-rounded developer with strong potential.</h1>
+                <h1 className='mx-3 text-center text-sm text-gray-300 md:text-left md:self-end md:mb-9 lg:w-[750px]'>Having good communication skills can lead to better collaboration and team cohesiveness. Being self-aware of one&apos;s strengths and weaknesses can improve one&apos;s accountability skills. And the result is a well-rounded developer with strong potential.</h1>
                 <SkillCard title="Soft Skills" image="/softSkills.png" bg={true}/>
             </div>
         </>
